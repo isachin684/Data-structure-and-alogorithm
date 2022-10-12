2ce0c5a45c2c19d3eab16780a2eae3aef64b11ad
Sachin Gupta <98533811+isachin684@users.noreply.github.com>
2022-10-12 14:54:37 +0530

    Create stackusingarray.c

---
+++ stackusingarray.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define SIZE 10
+void push(int);
+void pop();
+void display();int stack[SIZE], top = -1;
+void main()
+{
+int value, choice;
+printf("\nstack Using Array\n");
+while(1){printf("1. Push\n2. Pop\n3. Display");
+printf("\nEnter your choice: ");
+scanf("%d",&choice);
+switch(choice){
+case 1: printf("Enter the value to be insert: ");
+scanf("%d",&value);
+push(value);
+break;
+case 2: pop();
+break;
+case 3: display();
+break;
+
+}
+}
+}
+void push(int value){
+if(top == SIZE-1)
+printf("\nStack is Full!!! Insertion is not possible!!!");
+else{
+top++;
+stack[top] = value;
+printf("\nInsertion success!!!");
+}
+}
+void pop(){
+if(top == -1)
+printf("\nStack is Empty!!! Deletion is not possible!!!");
+else{
+printf("\nDeleted : %d", stack[top]);
+top--;
+}
+}
+void display(){
+if(top == -1)
+printf("\nStack is Empty!!!");
+else{
+int i;
+printf("\nStack elements are:\n");
+for(i=top; i>=0; i--)
+printf("%d\n",stack[i]);
+}
+}
